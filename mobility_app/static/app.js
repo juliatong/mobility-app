4896aabdd0f47a5381f970da8f0f8b71fadbd7a1
Julia Tong <juliatong2014thl@foxmail.com>
2021-08-07 00:58:01 +0800

    Utilize new APIs for filtering the region list

--- mobility_app/static/app.js
+++ mobility_app/static/app.js
@@ -35,4 +35,18 @@ $(document).ready(function(){
   const defaultData = {data:[]}
   $('#json_source').val(JSON.stringify(defaultData, undefined, 2));
   M.textareaAutoResize($('#json_source'));
+
+  $('#geoTypeSelector').on('change', function(e) {
+    $.get('/api/regions?geoType=' + $(this).val(), function(data, status){
+      if (status === 'success') {
+        console.log(data)
+        $el = $('#regionSelector')
+        $el.empty().append($("<option></option>").attr('disabled', true).attr('selected', true).text('Choose one'));
+        data.regions.forEach((region) => {
+          $el.append($("<option></option>").attr("value", region.id).text(region.name));
+        })
+        $('select').formSelect();
+      }
+    });
+  })
 });
