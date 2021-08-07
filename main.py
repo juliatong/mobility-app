37269bba02de62947c8006ca59c827b15b7f76e9
Julia Tong <juliatong2014thl@foxmail.com>
2021-08-07 18:41:04 +0800

    Add placeholder Pydantic models

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
+from typing import Optional
+
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+
+class Region(BaseModel):
+  id: int
+  geo_type: str
+  name: str
+
+
+class Location(BaseModel):
+  id: int
+  region: Region
+  sub_region: Optional[str] = None
+  country: Optional[str] = None
+
+
+class DirectionRequest(BaseModel):
+  location: Location
+  transportation_type: str
+  request_date: str
+  requests: float
+  locked: bool
+
 
 app = FastAPI()
 
@@ -27,4 +51,9 @@ def read_locations(region_id: int):
 
 @app.get("/api/traffic")
 def read_traffic():
+  return {'locations': []}
+
+
+@app.put("/api/traffic")
+def update_traffic():
   return {'locations': []}
