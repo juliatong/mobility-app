3f30997e6d7923b0b734003d3a55f4bf0c092761
Julia Tong <juliatong2014thl@foxmail.com>
2021-08-07 11:54:37 +0800

    Use proper way to define debug mode

--- mobility_app/__init__.py
+++ mobility_app/__init__.py
@@ -9,8 +9,6 @@ mobility_app.register_blueprint(backend_blueprint, url_prefix='/api')
 
 from mobility_app import views
 
-mobility_app.config.from_object('config')
-
 from dotenv import load_dotenv
 
 load_dotenv()
