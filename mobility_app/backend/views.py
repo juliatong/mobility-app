ea29a82eeec05533d66d32eb628d1e47da0765f5
Julia Tong <juliatong2014thl@foxmail.com>
2021-08-07 11:52:30 +0800

    First cut at getting traffic payload

--- mobility_app/backend/views.py
+++ mobility_app/backend/views.py
@@ -28,3 +28,44 @@ def locations():
                           for i, value in enumerate(row)) for row in cursor.fetchall()]
 
   return jsonify({'locations': locations_list})
+
+
+@backend_blueprint.route('/traffic', methods=['POST'])
+def traffic():
+  geo_type = request.form['geoType']
+  region_id = request.form['region']
+  # show_transit = request.form['showTransit']
+  # show_driving = request.form['showDriving']
+  # show_walking = request.args['showWalking']
+  # traffic_filter_modifier = request.args['trafficFilterModifier']
+  # traffic_filter_amt = request.args['trafficFilterAmt']
+
+  if geo_type and region_id:
+    with current_app.config.mysql.get_db().cursor() as cursor:
+      sql = '''
+        SELECT `locations`.`id`, `regions`.`name` as `region_name`, `regions`.`geo_type`, `locations`.`sub_region`, `locations`.`country`, `locations`.`alternative_name`
+        FROM `locations` INNER JOIN `regions` ON `locations`.`region_id` = `regions`.`id`
+        '''
+
+      where_statements = []
+      where_conditions = {}
+      if (geo_type != 'all'):
+        where_statements.append('`regions`.`geo_type` = %(geo_type)s')
+        where_conditions['geo_type'] = geo_type
+
+      if (region_id != 'all'):
+        where_statements.append('`locations`.`region_id` = %(region_id)s')
+        where_conditions['region_id'] = region_id
+
+      if len(where_statements) > 0:
+        sql += 'WHERE'
+        sql += ' AND '.join(where_statements)
+
+      cursor.execute(sql, where_conditions)
+
+      locations_list = [dict((cursor.description[i][0], value)
+                            for i, value in enumerate(row)) for row in cursor.fetchall()]
+
+      return jsonify({'locations':locations_list})
+  else:
+    return jsonify({'error':'Enter the required fields'})
--- mobility_app/static/app.js
+++ mobility_app/static/app.js
@@ -27,26 +27,51 @@ const myChart = new Chart(
   config
 );
 
+const populateDataSource= function(data) {
+  $('#dataSource').val(JSON.stringify(data, undefined, 2));
+  M.textareaAutoResize($('#dataSource'));
+}
+
 $(document).ready(function(){
   M.updateTextFields();
   $('.collapsible').collapsible();
   $('select').formSelect();
 
   const defaultData = {data:[]}
-  $('#json_source').val(JSON.stringify(defaultData, undefined, 2));
-  M.textareaAutoResize($('#json_source'));
+  populateDataSource(defaultData)
 
-  $('#geoTypeSelector').on('change', function(e) {
+  $('#geoTypeSelector').on('change', function() {
     $.get('/api/regions?geoType=' + $(this).val(), function(data, status){
       if (status === 'success') {
         console.log(data)
         $el = $('#regionSelector')
-        $el.empty().append($("<option></option>").attr('disabled', true).attr('selected', true).text('Choose one'));
+        $el.empty();
+        $el.append($('<option></option>').attr('disabled', true).text('Choose one'));
+        $el.append($('<option value="all">Select All</option>').attr('selected', true));
         data.regions.forEach((region) => {
-          $el.append($("<option></option>").attr("value", region.id).text(region.name));
+          $el.append($('<option></option>').attr('value', region.id).text(region.name));
         })
         $('select').formSelect();
       }
     });
+  })
+
+  $('#filter-form').on('submit', function(e) {
+    e.preventDefault();
+    $.ajax({
+        url: '/api/traffic',
+        data: $('#filter-form').serialize(),
+        type: 'POST',
+        success: function(response) {
+          populateDataSource(response.locations)
+        },
+        error: function(error) {
+          console.error(error);
+        }
+    });
+  });
+
+  $('#publish-form').on('submit', function(e) {
+    e.preventDefault();
   })
 });
