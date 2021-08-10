2608fc265fe31aa2fdd4d68a9e074a66e5e06adf
Julia Tong <juliatong2014thl@foxmail.com>
2021-08-11 00:04:07 +0800

    Implement features

--- main.py
+++ main.py
@@ -10,30 +10,46 @@ from pydantic import BaseModel
 from sqlalchemy import create_engine
 
 
-class Region(BaseModel):
-  id: int
-  geo_type: str
-  name: str
-
-
-class Location(BaseModel):
-  id: int
-  region: Region
-  sub_region: Optional[str] = None
-  country: Optional[str] = None
-
-
-class DirectionRequest(BaseModel):
-  location: Location
-  transportation_type: str
-  request_date: str
-  requests: float
-  locked: bool
-
-
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
+
+engine = create_engine('sqlite:///mobility_data.db')
+
+
+def get_traffic_from_transportation_type(traffic_data_frame: DataFrame):
+    if traffic_data_frame.empty:
+        traffic_data_frame = {}
+    else:
+        traffic_data_frame = traffic_data_frame.reset_index(drop=True)
+        traffic_data_frame = traffic_data_frame.drop(columns=['index',
+                                                              'geo_type',
+                                                              'region',
+                                                              'transportation_type',
+                                                              'alternative_name',
+                                                              'sub-region',
+                                                              'country',
+                                                              'lock']
+                                                     )
+        traffic_data_frame = traffic_data_frame.fillna('')
+        traffic_data_frame = traffic_data_frame.T.to_dict()[0]
+
+    return traffic_data_frame
+
+
+@app.post("/load-csv")
+def load_csv(request: Request):
+    df = pd.read_csv('resources/applemobilitytrends-2021-07-31.csv')
+    #  Write Regions Table
+    regions_table = df \
+        .sort_values(by='region') \
+        .reset_index(drop=True)
+
+    regions_table['lock'] = False
+
+    regions_table.to_sql("regions", engine, if_exists='replace')
+
+    return {'response': 'success'}
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -43,14 +59,276 @@ def read_root(request: Request):
 
 @app.get("/api/regions/{geo_type}")
 def read_regions(geo_type: str):
-  return {'geoType': geo_type, 'regions': []}
+    # Probably should replace this with a live DB check
+    if geo_type not in ["country", "city", "sub-region", "county"]:
+        return {'error': 'GeoType not found'}
+
+    if geo_type == "country":
+        geo_type = "country/region"
+
+    regions_table = pd.read_sql_query(
+        f"SELECT region, `sub-region`, country FROM regions WHERE geo_type = '{geo_type}' ORDER BY region",
+        engine)
+
+    regions_table = regions_table.drop_duplicates(['region', 'sub-region', 'country'])
+
+    regions = regions_table.to_dict(orient='records')
+
+    return {'geoType': geo_type, 'regions': regions}
+
+
+def _read_traffic(region: str,
+                  sub_region: str = '',
+                  transit: bool = False,
+                  driving: bool = False,
+                  walking: bool = False):
+    if sub_region:
+        regions_table = pd.read_sql_query(f"SELECT * FROM regions "
+                                          f"WHERE region = '{region}' "
+                                          f"AND `sub-region` = '{sub_region}'",
+                                          engine)
+    else:
+        regions_table = pd.read_sql_query(f"SELECT * FROM regions "
+                                          f"WHERE region = '{region}' "
+                                          f"AND `sub-region` IS NULL",
+                                          engine)
+
+    if regions_table.empty:
+        return {
+            'error': f'Region {region} with Sub Region {sub_region} not found'
+        }
+
+    if transit:
+        traffic = regions_table.loc[regions_table['transportation_type'] == 'transit']
+        transit = get_traffic_from_transportation_type(traffic)
+
+    if driving:
+        traffic = regions_table.loc[regions_table['transportation_type'] == 'driving']
+        driving = get_traffic_from_transportation_type(traffic)
+
+    if walking:
+        traffic = regions_table.loc[regions_table['transportation_type'] == 'walking']
+        walking = get_traffic_from_transportation_type(traffic)
+
+    first_record = regions_table.iloc[0]
+
+    return {
+        "regions": [
+            {
+                "alternative_name": first_record['alternative_name'],
+                "country": first_record['country'],
+                "geo_type": first_record['geo_type'],
+                "region_name": first_record['region'],
+                "sub_region": first_record['sub-region'],
+                "direction_requests": {
+                    "transit": transit,
+                    "driving": driving,
+                    "walking": walking
+                }
+            }
+        ]
+    }
 
 
 @app.get("/api/traffic")
-def read_traffic():
-  return {'locations': []}
+def read_traffic(region: str,
+                 sub_region: str = '',
+                 transit: bool = False,
+                 driving: bool = False,
+                 walking: bool = False):
+    return _read_traffic(region, sub_region, transit, driving, walking)
+
+
+class UpdateTrafficPayload(BaseModel):
+    region: str
+    sub_region: Optional[str] = ''
+    traffic: dict
 
 
 @app.put("/api/traffic")
-def update_traffic():
-  return {'locations': []}
+def update_traffic(traffic_payload: UpdateTrafficPayload):
+    update_data = []
+
+    if 'transit' in traffic_payload.traffic:
+        transit_data = traffic_payload.traffic['transit']
+        update_data.append((transit_data, 'transit'))
+    else:
+        transit_data = {}
+
+    if 'driving' in traffic_payload.traffic:
+        driving_data = traffic_payload.traffic['driving']
+        update_data.append((driving_data, 'driving'))
+    else:
+        driving_data = {}
+
+    if 'walking' in traffic_payload.traffic:
+        walking_data = traffic_payload.traffic['walking']
+        update_data.append((walking_data, 'walking'))
+    else:
+        walking_data = {}
+
+    if traffic_payload.sub_region:
+        region_data = pd.read_sql_query(f"SELECT "
+                                        f"* FROM regions "
+                                        f"WHERE region = '{traffic_payload.region}' "
+                                        f"AND `sub-region` = '{traffic_payload.sub_region}' "
+                                        f"AND lock = 0",
+                                        engine)
+    else:
+        region_data = pd.read_sql_query(f"SELECT "
+                                        f"* FROM regions "
+                                        f"WHERE region = '{traffic_payload.region}' "
+                                        f"AND `sub-region` IS NULL "
+                                        f"AND lock = 0",
+                                        engine)
+
+    # Combine the data with non data columns
+    # Delete the original data
+    # Append the new data
+
+    with engine.connect() as con:
+
+        for data in update_data:
+            if data[0]:
+                traffic_dataframe = region_data[region_data.transportation_type == data[1]]
+                traffic_dict = traffic_dataframe.to_dict(orient='records')
+                traffic_dict[0].update(data[0])
+
+                con.execute(f"DELETE FROM regions WHERE `index` = '{traffic_dataframe.iloc[0]['index']}'")
+
+                new_traffic_dataframe = pd.DataFrame.from_dict(traffic_dict)
+                new_traffic_dataframe = new_traffic_dataframe.set_index('index')
+                new_traffic_dataframe.to_sql('regions', engine, if_exists='append')
+
+    return _read_traffic(traffic_payload.region, traffic_payload.sub_region, transit_data, driving_data, walking_data)
+
+
+@app.get("/api/traffic/filter")
+def filtered_traffic(transit: bool = False,
+                     driving: bool = False,
+                     walking: bool = False,
+                     traffic_filter_type: str = 'gt',
+                     traffic_filter_amount: int = 0):
+    transportation_type = []
+
+    if traffic_filter_type not in ['lt', 'gt']:
+        return {'error', 'traffic_filter_type must be lt or gt'}
+
+    if transit:
+        transportation_type.append('transit')
+    if driving:
+        transportation_type.append('driving')
+    if walking:
+        transportation_type.append('walking')
+
+    if len(transportation_type) > 1:
+        transportation_type = tuple(transportation_type)
+    elif len(transportation_type) == 1:
+        transportation_type = f"('{transportation_type[0]}')"
+    else:
+        return {"error": "No Transportation Type Selected"}
+
+    regions_table = pd.read_sql_query(
+        f"SELECT * FROM regions WHERE transportation_type in {transportation_type}",
+        engine)
+
+    data_only_regions_table = regions_table.drop(columns=['index',
+                                                          'geo_type',
+                                                          'region',
+                                                          'transportation_type',
+                                                          'alternative_name',
+                                                          'sub-region',
+                                                          'country',
+                                                          'lock'])
+    data_only_regions_table = data_only_regions_table.fillna(0)
+
+    if traffic_filter_type == 'gt':
+        data_only_regions_table = data_only_regions_table[
+            (data_only_regions_table.values > traffic_filter_amount).any(1)]
+    else:
+        data_only_regions_table = data_only_regions_table[
+            (data_only_regions_table.values < traffic_filter_amount).any(1)]
+
+    filtered_region_data = regions_table.loc[regions_table.index[data_only_regions_table.index]]
+
+    filtered_regions = filtered_region_data[['region',
+                                             'sub-region',
+                                             'country',
+                                             'alternative_name',
+                                             'geo_type']] \
+        .drop_duplicates(['region', 'sub-region', 'country'])
+
+    output_dict = {
+        "regions": [
+
+        ]
+    }
+
+    for index, row in filtered_regions.iterrows():
+
+        traffic_data = filtered_region_data[(filtered_region_data['region'] == row['region'])]
+        transit_data = dict()
+        driving_data = dict()
+        walking_data = dict()
+
+        if transit:
+            transit_data = traffic_data[traffic_data['transportation_type'] == 'transit']
+            transit_data = get_traffic_from_transportation_type(transit_data)
+        if driving:
+            driving_data = traffic_data[traffic_data['transportation_type'] == 'driving']
+            driving_data = get_traffic_from_transportation_type(driving_data)
+        if walking:
+            walking_data = traffic_data[traffic_data['transportation_type'] == 'walking']
+            walking_data = get_traffic_from_transportation_type(walking_data)
+
+        output_dict['regions'].append({
+            "alternative_name": row['alternative_name'],
+            "country": row['country'],
+            "geo_type": row['geo_type'],
+            "region_name": row['region'],
+            "sub_region": row['sub-region'],
+            "direction_requests": {
+                "transit": transit_data,
+                "driving": driving_data,
+                "walking": walking_data
+            }
+        })
+
+    return output_dict
+
+
+class LockTrafficPayload(BaseModel):
+    region: str
+    sub_region: Optional[str] = ''
+    transportation_type: str
+    lock: bool = False
+
+
+@app.post("/api/traffic/lock")
+def lock_traffic(lock_payload: LockTrafficPayload):
+    print(lock_payload)
+
+    if not lock_payload.transportation_type:
+        return {'error': 'transportation_type must be a comma separated string'}
+
+    transportation_type = lock_payload.transportation_type.split(',')
+
+    with engine.connect() as con:
+        for transportation_type in transportation_type:
+            transportation_type = transportation_type.strip()
+            if transportation_type not in ['transit', 'driving', 'walking']:
+                return {'error': transportation_type}
+            if lock_payload.sub_region:
+                sql_sub_region = f"`sub-region` = '{lock_payload.sub_region}'"
+            else:
+                sql_sub_region = f'`sub-region` IS NULL'
+            con.execute(f"UPDATE regions "
+                        f"SET lock = {int(lock_payload.lock)} "
+                        f"WHERE region = '{lock_payload.region}' "
+                        f"AND {sql_sub_region} "
+                        f"AND transportation_type = '{transportation_type}'")
+
+            return {
+                'region': lock_payload.region,
+                'lock': int(lock_payload.lock)
+            }
--- static/app.js
+++ static/app.js
@@ -45,15 +45,249 @@ const config = {
   }
 }
 
-const myChart = new Chart(
-  document.getElementById('myChart'),
-  config
-);
+let currentRegions = []
+let datasetList = []
+let selectedDataset = []
 
-const populateDataSource= function(data) {
-  $('#dataSource').val(JSON.stringify(data, undefined, 2));
-  M.textareaAutoResize($('#dataSource'));
+const dataChart = new Chart(
+  document.getElementById('dataChart'),
+  config
+)
+
+const buildRegionLabel = function(region) {
+  let regionLabel = ''
+  if (region.region_name) {
+    regionLabel = `${region.region_name}`
+
+    if (region.sub_region) {
+      regionLabel = `${region.region_name}, ${region.sub_region}`
+    }
+  } else {
+    regionLabel = `${region.region}`
+
+    if (region['sub-region']) {
+      regionLabel = `${region.region}, ${region['sub-region']}`
+    }
+  }
+
+  if (region.country) {
+    regionLabel = `${regionLabel}, ${region.country}`
+  }
+
+  return regionLabel
 }
+
+const buildDatasetLabel = function(row, dataType) {
+  let datasetLabel = `${row.region_name}`
+
+  if (row.sub_region) {
+    datasetLabel = `${row.region_name}, ${row.sub_region}`
+  }
+
+  if (row.country) {
+    datasetLabel = `${datasetLabel}, ${row.country}`
+  }
+
+  return `${dataType} (${datasetLabel})`
+}
+
+const buildDataSet = function(row, data, label) {
+  dataChart.data.datasets.push(
+    {
+      label: buildDatasetLabel(row, label),
+      borderColor: namedColor(dataChart.data.datasets.length),
+      data: Object.entries(data).map(([key, value]) => {
+        return {x: key, y: value}
+      }),
+    }
+  )
+  dataChart.update()
+}
+
+const resetData = function(chart) {
+  $('#currentDatasetLabel').empty()
+  while(chart.data.datasets.length > 0) {
+    chart.data.datasets.pop();
+  }
+  chart.update()
+}
+
+const toggleDataset = function(index) {
+  let indexPos = selectedDataset.indexOf(index)
+
+  if (indexPos < 0) {
+    selectedDataset.push(index)
+  } else {
+    delete selectedDataset[indexPos]
+  }
+
+  selectedDataset = selectedDataset.flat()
+
+  resetData(dataChart)
+
+  selectedDataset.forEach( idx => {
+    let region = datasetList[idx]
+    setCurrentDataset(region)
+  })
+
+  const selectedRegions = selectedDataset.map( idx => {
+    return datasetList[idx]
+  })
+
+  $('#dataSource').val(JSON.stringify(selectedRegions, undefined, 2))
+  M.textareaAutoResize($('#dataSource'))
+}
+
+const setCurrentDataset = function(region) {
+  // $('#currentDatasetLabel').text(`(${buildRegionLabel(region)})`)
+
+  if (region.direction_requests.transit && Object.values(region.direction_requests.transit).length > 0) {
+    buildDataSet(region, region.direction_requests.transit, 'Transit')
+  }
+
+  if (region.direction_requests.driving && Object.values(region.direction_requests.driving).length > 0) {
+    buildDataSet(region, region.direction_requests.driving, 'Driving')
+  }
+
+  if (region.direction_requests.walking && Object.values(region.direction_requests.walking).length > 0) {
+    buildDataSet(region, region.direction_requests.walking, 'Walking')
+  }
+}
+
+const populateDatasetList = function() {
+  $el = $('#dataset-list')
+  $el.empty()
+
+  for (let i = 0; i < datasetList.length; i++) {
+    const region = datasetList[i]
+
+    let $input = $(`<input type="checkbox" class="filled-in dataset-selector" name="selectedDataset${i}" id="selectedDataset${i}" value="${i}" />`)
+    $input.on('click', function(e) {
+      let idx = parseInt($(e.target).val())
+      console.log('Toggling Dataset', idx)
+      toggleDataset(idx)
+    })
+    let $datasetSelector = $('<label></label>').append($input)
+    $('<span></span>').insertAfter($input)
+
+    let $cell = $('<p></p>').append($datasetSelector)
+    let $action = $('<td></td>').append($cell)
+
+    let row = $(`<tr><td>${region.geo_type}</td><td>${buildRegionLabel(region)}</td></tr>`)
+    row.append($action)
+
+    $el.append(row)
+  }
+}
+
+const populateDataSource = function(data, transportTypeToDisplay = { transit: true, walking: true, driving: true }) {
+  data.forEach( row => {
+    if (!transportTypeToDisplay.transit) {
+      delete row.direction_requests['transit']
+    }
+
+    if (!transportTypeToDisplay.driving) {
+      delete row.direction_requests['driving']
+    }
+
+    if (!transportTypeToDisplay.walking) {
+      delete row.direction_requests['walking']
+    }
+  })
+
+  datasetList = data
+
+  populateDatasetList()
+  resetData(dataChart)
+  selectedDataset = []
+
+  if (datasetList.length > 0) {
+    toggleDataset(0)
+    $('#selectedDataset0').attr('checked', 'checked')
+  }
+}
+
+const isFormFieldChecked = function(form, fieldName) {
+  return form.serializeArray().filter( field => field.name == fieldName).length > 0
+}
+
+const transportTypeToDisplay = function(form) {
+  return {
+    transit: isFormFieldChecked(form, 'showTransit'),
+    walking: isFormFieldChecked(form, 'showWalking'),
+    driving: isFormFieldChecked(form, 'showDriving'),
+  }
+}
+
+const validateData = function(regions) {
+  let shouldPrompt = false
+
+  let maxAllowable = parseInt($('#maxTrafficRequestAllowed').val())
+  if ( isNaN(maxAllowable)) {
+    maxAllowable = 3000
+  }
+
+  regions.forEach( regionData => {
+    ['transit', 'driving', 'walking'].forEach( trafficType => {
+      if (regionData.direction_requests[trafficType]) {
+        Object.values(regionData.direction_requests[trafficType]).forEach( dataPoint => {
+          if (dataPoint > maxAllowable) {
+            shouldPrompt = true
+          }
+        })
+      }
+    })
+  })
+
+  if (shouldPrompt) {
+    return confirm(`There are traffic values that are higher than the maximum allowable limit of ${maxAllowable}.\n\nDo you want to allow this?`)
+  }
+
+  return true
+}
+
+const lockDataset = function(regionData) {
+  try {
+    const transportationType = []
+
+    const transportationTypes = ['transit', 'driving', 'walking']
+    transportationTypes.forEach( trafficType => {
+      if (regionData.direction_requests[trafficType]) {
+        transportationType.push(trafficType)
+      }
+    })
+
+    if (transportationType.length < 1) {
+      return
+    }
+
+    const formData = {
+      region: regionData.region_name,
+      sub_region: regionData.sub_region,
+      transportation_type: transportationType.join(','),
+      lock: true
+    }
+
+    $.ajax({
+      url: '/api/traffic/lock',
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify(formData),
+      success: function(response) {
+        console.log('Locked Data for:', trafficType)
+      },
+      error: function(error) {
+        console.error(error)
+        alert(error.message)
+      }
+    })
+  }
+  catch(err) {
+    console.error(err)
+    alert(err.message)
+  }
+}
+
 
 $(document).ready(function(){
   M.updateTextFields();
@@ -66,15 +300,19 @@ $(document).ready(function(){
   populateDataSource(defaultData)
 
   $('#geoTypeSelector').on('change', function() {
-    $.get('/api/regions?geoType=' + $(this).val(), function(data, status){
+    $.get('/api/regions/' + $(this).val(), function(data, status){
       if (status === 'success') {
         $el = $('#regionSelector')
         $el.empty();
-        $el.append($('<option></option>').attr('disabled', true).text('Choose one'));
-        $el.append($('<option value="all">Select All</option>').attr('selected', true));
-        data.regions.forEach((region) => {
-          $el.append($('<option></option>').attr('value', region.id).text(region.name));
-        })
+        $el.append($('<option></option>').attr('disabled', true).attr('selected', true).text('Choose one'));
+
+        currentRegions = data.regions
+
+        for (let i = 0; i < currentRegions.length; i++) {
+          const region = currentRegions[i]
+          $el.append($('<option></option>').attr('value', i).text(buildRegionLabel(region)))
+        }
+
         $('select').formSelect();
       } else {
         console.error(status)
@@ -82,22 +320,134 @@ $(document).ready(function(){
     });
   })
 
-  $('#filter-form').on('submit', function(e) {
+  $('#filter-region-form').on('submit', function(e) {
     e.preventDefault();
-    $.ajax({
+
+    try {
+      if ( $('#regionSelector').val() == null || $('#regionSelector').val().length === 0 ) {
+        throw Error('Please select a region.')
+      }
+
+      const regionIndex = parseInt($('#regionSelector').val())
+      const region = currentRegions[regionIndex]
+
+      let searchParams = {
+        region: region.region,
+        sub_region: region['sub-region']
+      }
+      searchParams = Object.assign(searchParams, transportTypeToDisplay($('#filter-region-form')))
+
+      $.ajax({
         url: '/api/traffic',
-        data: $('#filter-form').serialize(),
-        type: 'POST',
+        data: searchParams,
+        type: 'GET',
         success: function(response) {
-          populateDataSource(response.locations)
+          populateDataSource(response.regions, transportTypeToDisplay($('#filter-region-form')))
         },
         error: function(error) {
-          console.error(error);
+          console.error(error)
+          alert(error.message)
+        }
+      });
+    }
+    catch(err) {
+      alert(err.message)
+    }
+  })
+
+  $('#filter-traffic-form').on('submit', function(e) {
+    e.preventDefault();
+    $('#filter-traffic-form-loader').removeClass('hide')
+
+    let searchParams = {
+      traffic_filter_type: $('#trafficFilterType').val(),
+      traffic_filter_amount: $('#trafficFilterAmt').val()
+    }
+    searchParams = Object.assign(searchParams, transportTypeToDisplay($('#filter-traffic-form')))
+
+    try {
+      $.ajax({
+        url: '/api/traffic/filter',
+        data: searchParams,
+        type: 'GET',
+        success: function(response) {
+          populateDataSource(response.regions, transportTypeToDisplay($('#filter-traffic-form')))
+          $('#filter-traffic-form-loader').addClass('hide')
+        },
+        error: function(error) {
+          console.error(error)
+          alert(error.message)
         }
     });
-  });
+    }
+    catch(err) {
+      $('#filter-traffic-form-loader').addClass('hide')
+      alert(err.message)
+    }
+  })
+
+
+  $('#updateChart').on('click', function(e) {
+    e.preventDefault();
+
+    try {
+      const jsonData = JSON.parse($('#dataSource').val())
+
+      for (let i = 0; i < selectedDataset.length; i++) {
+        const key = selectedDataset[i]
+        datasetList[key] = jsonData[i]
+      }
+
+      orgSelectedDataset = [...selectedDataset]
+      selectedDataset = []
+
+      orgSelectedDataset.forEach( idx => {
+        toggleDataset(idx)
+      })
+    }
+    catch(err) {
+      console.error(err)
+      alert(err.message)
+    }
+  })
 
   $('#publish-form').on('submit', function(e) {
     e.preventDefault();
+
+    try {
+      const jsonData = JSON.parse($('#dataSource').val())
+
+      if (validateData(jsonData)) {
+        jsonData.forEach( regionData => {
+          let formData = {
+            region: regionData.region_name,
+            sub_region: regionData.sub_region,
+            traffic: regionData.direction_requests
+          }
+
+          $.ajax({
+            url: '/api/traffic',
+            type: 'PUT',
+            contentType: 'application/json',
+            data: JSON.stringify(formData),
+            success: function(_) {
+              if (isFormFieldChecked($('#publish-form'), 'lockData')) {
+                lockDataset(regionData)
+                alert(`New data for ${regionData.region_name} has been published and locked.`)
+              } else {
+                alert(`New data for ${regionData.region_name} has been published.`)
+              }
+            },
+            error: function(error) {
+              console.error(error)
+              alert(error.message)
+            }
+          })
+        })
+      }
+    }
+    catch(err) {
+      alert(err.message)
+    }
   })
 });
